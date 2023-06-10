bda07707d0a43fb309cde282a119b8435ae2abda
Liss <les.karo@yandex.ru>
2023-06-10 18:33:20 +0400

    стиль для NavbarAdmin

--- src/components/UI/Navbar/NavbarAdmin.jsx
+++ src/components/UI/Navbar/NavbarAdmin.jsx
@@ -1,41 +1,41 @@
 import React, {useContext} from 'react';
 import {Context} from "../../../index";
 import {useNavigate} from "react-router-dom";
-
+import cl from './Navbar.module.css'
 import {ADMIN_ROUTE, LOGIN_ROUTE} from "../../../utils/consts";
-import {Button, Container, Nav, Navbar, NavLink} from "react-bootstrap";
+import {Button, Nav} from "react-bootstrap";
+import MyButton from "../MyButton/MyButton";
+import {observer} from "mobx-react-lite";
 
-const NavbarAdmin = () => {
+const NavbarAdmin = observer(() => {
     const {admin} = useContext(Context)
     const navigate = useNavigate()
 
     return (
-        <Navbar bg="dark" variant="dark">
-            <Container>
-                {admin.isAuth ?
-                    <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button
-                            variant={'outline-light'}
-                            onClick={() => navigate(ADMIN_ROUTE)}
-                        >
-                            Админ панель
-                        </Button>
-                        <Button
-                            variant={'outline-light'}
-                            onClick={() => navigate(LOGIN_ROUTE)}
-                            className="ml-2"
-                        >
-                            Выйти
-                        </Button>
-                    </Nav>
-                    :
-                    <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={'outline-light'} onClick={() => admin.setIsAuth(true)}>Авторизация</Button>
-                    </Nav>
-                }
-            </Container>
-        </Navbar>
+        <div className={cl.navbar}>
+            {admin.isAuth ?
+                <Nav className="ml-auto" style={{color: 'white'}}>
+                    <MyButton
+                        variant={'outline-light'}
+                        onClick={() => navigate(ADMIN_ROUTE)}
+                    >
+                        Админ панель
+                    </MyButton>
+                    <MyButton
+                        variant={'outline-light'}
+                        onClick={() => navigate(LOGIN_ROUTE)}
+                        className="ml-2"
+                    >
+                        Выйти
+                    </MyButton>
+                </Nav>
+                :
+                <Nav className="ml-auto" style={{color: 'white'}}>
+                    <Button variant={'outline-light'} onClick={() => admin.setIsAuth(true)}>Авторизация</Button>
+                </Nav>
+            }
+        </div>
     );
-};
+});
 
 export default NavbarAdmin;
