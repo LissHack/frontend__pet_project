33eacbde2e9415a9873bcaa4e02daf7fb36b9ce0
Liss <les.karo@yandex.ru>
2023-06-11 14:11:55 +0400

    Настройка DevicePage

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import NavbarAdmin from "../../components/UI/Navbar/NavbarAdmin";
-
+import cl from './Admin.module.css'
+import MyButton from "../../components/UI/MyButton/MyButton";
 const Admin = () => {
     return (
         <div>
            <NavbarAdmin/>
+            <div className={cl.container__admin}>
+                <MyButton>Добавить работника</MyButton>
+                <MyButton>Добавить отдел</MyButton>
+                <MyButton>добавить должность</MyButton>
+            </div>
         </div>
     );
 };
--- src/pages/DevicePage/DevicePage.jsx
+++ src/pages/DevicePage/DevicePage.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import {Card, Col, Container, Row} from "react-bootstrap";
+import {Card, Col, Container, Image, Row} from "react-bootstrap";
 import NavbarStorage from "../../components/UI/Navbar/NavbarStorage";
-import TypeBar from "../../components/TypeBar";
-import DeviceList from "../../components/DeviceList";
+
 import cl from './DevicePage.module.css'
 
 const DevicePage = () => {
+    const device = {id: 1, name: '25BK550Y', SN: '910NTGY2X975', state_at: 'Б/У'}
+    const description = [
+        {id: 1, title: 'Оперативная память', description: '5 гб'},
+        {id: 2, title: 'Камера', description: '12 мп'},
+        {id: 3, title: 'Процессор', description: 'Пентиум 3'},
+        {id: 4, title: 'Аккумулятор', description: '4000'},
+    ]
+
     return (
         <div>
             <NavbarStorage/>
             <div className={cl.container__device_page}>
 
                 <Card>
-                    <DeviceList/>
+                    <Image className={cl.card__img} src={device.img}/>
+                </Card>
+                <Card className={cl.card__desc}>
+                    <h3>Характеристики</h3>
+                    {description.map((info, index) =>
+                        <Row key={info.id}
+                             style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                            {info.title}: {info.description}
+                        </Row>
+                    )}
                 </Card>
             </div>
         </div>
