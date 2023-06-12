2f0125cbebb20a74ab6271906133c1991a0d2800
Liss <les.karo@yandex.ru>
2023-06-12 14:24:11 +0400

    Создание модального окна для создания должности

--- src/components/UI/MyModal/CreatePosition.js
+++ src/components/UI/MyModal/CreatePosition.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import Modal from "react-bootstrap/Modal";
+import MyButton from "../MyButton/MyButton";
+import cl from './MyModal.module.css'
+import {Card} from "react-bootstrap";
+import MyInput from "../input/MyInput";
 import Form from "react-bootstrap/Form";
-import {Button} from "react-bootstrap";
 
 const CreatePosition = ({show, onHide}) => {
     return (
-         <Modal
-            show={show}
-            onHide={onHide}
-            size="lg"
-            centered
+        <Modal className={cl.container__modal}
+               show={show}
+               onHide={onHide}
+               size="lg"
+               centered
         >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Добавить должность
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form>
-                    <Form.Control
-                        placeholder={"Введите название должности"}
+            <Form className={cl.modal}>
+                <Card className={cl.content__modal}>
+                    <h3>Добавить должность</h3>
+                    <MyInput
+                        placeholder={'Введите название должности'}
                     />
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
-                <Button variant="outline-success" onClick={onHide}>Добавить</Button>
-            </Modal.Footer>
+                    <div className={cl.modal__btn}>
+                        <MyButton onClick={onHide}>Закрыть</MyButton>
+                        <MyButton onClick={onHide}>Добавить</MyButton>
+                    </div>
+                </Card>
+            </Form>
+
         </Modal>
     );
 };
