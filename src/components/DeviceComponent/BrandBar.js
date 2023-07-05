4f9b5e3c56bf04f814dba5d22eb18c9cc8e182a3
Liss <les.karo@yandex.ru>
2023-07-05 10:01:08 +0400

    Настройка выпадающего меню

--- src/components/DeviceComponent/BrandBar.js
+++ src/components/DeviceComponent/BrandBar.js
@@ -2,21 +2,29 @@ import React, {useContext} from 'react';
 import {observer} from "mobx-react-lite";
 import {Context} from "../../index";
 import cl from './BrandBar.module.css'
+import {Dropdown} from "react-bootstrap";
 
 const BrandBar = observer(() => {
     const {device} = useContext(Context)
 
     return (
-        <div className={cl.content__brand}>
-            {device.brands.map(brand =>
-                <card className={cl.card__brand}
-                      key={brand.id}
-                      onClick={() => device.setSelectedBrand(brand)}
-                >
-                    {brand.name}
-                </card>
-            )}
-        </div>
+        <Dropdown className={cl.dropdown}>
+            <Dropdown.Toggle className={cl.dropdown_btn}>
+                Бренд
+            </Dropdown.Toggle>
+            <Dropdown.Menu className={cl.dropdown_content}>
+                {device.brands.map(brand =>
+                    <Dropdown.Item
+                        className={cl.dropdown__condition}
+                        key={brand.id}
+                        onClick={() => device.setSelectedBrand(brand)}
+                    >
+                        {brand.name}
+                    </Dropdown.Item>
+                )}
+            </Dropdown.Menu>
+        </Dropdown>
+
     );
 });
 
--- src/components/DeviceComponent/ConditionBar.jsx
+++ src/components/DeviceComponent/ConditionBar.jsx
@@ -1,38 +1,33 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import cl from "./BrandBar.module.css";
 import {Context} from "../../index";
 import {Dropdown} from "react-bootstrap";
+import {fetchConditions} from "../../http/deviceAPI";
 
 const ConditionBar = () => {
     const {device} = useContext(Context)
+
+    useEffect(() => {
+        fetchConditions().then(data => device.setConditions(data))
+    }, [])
+
 
     return (
         <Dropdown className={cl.dropdown}>
             <Dropdown.Toggle className={cl.dropdown_btn}>
                 Состояние
-            </Dropdown.Toggle >
+            </Dropdown.Toggle>
             <Dropdown.Menu className={cl.dropdown_content}>
                 {device.conditions.map(condition =>
                     <Dropdown.Item className={cl.dropdown__condition}
-                          key={condition.id}
-                          onClick={() => device.setSelectedCondition(condition)}
+                                   key={condition.id}
+                                   onClick={() => device.setSelectedCondition(condition)}
                     >
                         {condition.name}
                     </Dropdown.Item>
                 )}
             </Dropdown.Menu>
         </Dropdown>
-
-        // <div className={cl.content__condition}>
-        //     {device.conditions.map(condition =>
-        //         <card className={cl.card__condition}
-        //               key={condition.id}
-        //               onClick={() => device.setSelectedCondition(condition)}
-        //         >
-        //             {condition.name}
-        //         </card>
-        //     )}
-        // </div>
     );
 };
 
